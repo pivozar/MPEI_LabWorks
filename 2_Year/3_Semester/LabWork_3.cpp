249cae4d2ac27043b1d5805973b4b3b3082291af
gregs0n <volkovpy@mpei.ru>
2020-09-18 22:24:34 +0300

    LabWork_3 begined

--- 2_Year/3_Semester/LabWork_3.cpp
+++ 2_Year/3_Semester/LabWork_3.cpp
@@ -7,9 +7,38 @@
  * При выполнении этого задания необходимо преобразовать текстовый файл в двоичный,
  * а также вывести на экран содержимое двоичного файла до обработки и после обработки.
  * Не забывайте закрывать файлы.
+ * Каждая запись представляет собой 2 числа типа float
 */
 
-int main(int argc, argv[]) {
+int ReadFloat(float *a, float *b, char* filename) {
+    FILE *file;
 
+    if ((file = fopen(filename, "r")) == NULL) {
+        printf("Impossible to open file '%s'\n", filename);
+        return 0;
+    }
+    if (scanf("%f %f", a, b) < 1) {
+        printf("Impossible to read from file: '%s'\n", filename);
+        return 0;
+    }
+    fclose(file);
+    return 1;
+}
+
+int WriteFloatBinary(float *a, float *b, char* filename) {
+    FILE *file;
+
+    if ((file = fopen(filename, "wb")) == NULL) {
+        printf("Impossible to write to file: '%s'\n", filename);
+        return 0;
+    }
+    if (fwrite(
+
+int main(int argc, char* argv[]) {
+    float a, b;
+    
+    if (!ReadFloat(&a, &b, argv[1]))
+        return 0;
+    
     return 0;
 }
