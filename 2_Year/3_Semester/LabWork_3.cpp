a178c210c804c1d98a5b5effb08772b652f2d069
gregs0n <volkovpy@mpei.ru>
2020-09-19 10:42:52 +0300

    LabWork_3 Continued

--- 2_Year/3_Semester/LabWork_3.cpp
+++ 2_Year/3_Semester/LabWork_3.cpp
@@ -10,35 +10,63 @@
  * Каждая запись представляет собой 2 числа типа float
 */
 
-int ReadFloat(float *a, float *b, char* filename) {
-    FILE *file;
+const int NMAX = 100;
 
+int ReadArray(int *arr_size, float arr[], char *filename);
+int WriteArrayBinary(int arr_size, float arr[], filename);
+int PrintArray(int arr_size, float arr[], filename);
+int SwapInFile(int pos1, int pos2, filename);
+
+int main(int argc, char* argv[]) {
+    float array[NMAX];
+    int arr_size;
+    int pos1, pos2;
+
+    if (argc < 2) {
+        printf("More arguments required!\n");
+        return 1;
+    }
+    if (!ReadArray(&arr_size, array, argv[1]))
+        return 1;
+
+    WriteArrayBinary(arr_size, array, filename);
+    printf("Array before changes:\n");
+    PrintArray(arr_size, array, filename);
+
+    pintf("Enter positions of elements to swap:\n");
+    scanf("%d%d", &pos1, &pos2);
+    
+    SwapInFile(pos1, pos2, filename);
+    
+
+    return 0;
+}
+
+int ReadArray(int *arr_size, float arr[], char *filename) {
+    FILE *file;
+    
     if ((file = fopen(filename, "r")) == NULL) {
         printf("Impossible to open file '%s'\n", filename);
         return 0;
     }
-    if (scanf("%f %f", a, b) < 1) {
-        printf("Impossible to read from file: '%s'\n", filename);
+
+    if (fscanf(file, "%d", arr_size) < 1) {
+        printf("Error while reading file '%s'", filename);
         return 0;
+    }
+
+    if (*arr_size < 0 || *arr_size > NMAX) {
+        printf("Array size must be from 1 to %d! (file: '%s')\n", NMAX, filename);
+        return 0;
+    }
+
+    for (int i = 0; i < *arr_size; ++i) {
+        if (fscanf(file,  "%lf", &arr[i]) < 1) {
+            printf("Error while reading file '%s'", filename);
+            return 0;
+        }
     }
     fclose(file);
     return 1;
 }
 
-int WriteFloatBinary(float *a, float *b, char* filename) {
-    FILE *file;
-
-    if ((file = fopen(filename, "wb")) == NULL) {
-        printf("Impossible to write to file: '%s'\n", filename);
-        return 0;
-    }
-    if (fwrite(
-
-int main(int argc, char* argv[]) {
-    float a, b;
-    
-    if (!ReadFloat(&a, &b, argv[1]))
-        return 0;
-    
-    return 0;
-}
